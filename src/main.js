1dda1cc0fd082aa6ff47403efe4d82de5612a86b
Henry Beveridge <henrydbeveridge@gmail.com>
2025-04-22 21:41:44 -0700

    2d controls

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import "./style.css";
 
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-
-let paused = false;
+import { MapControls } from "three/examples/jsm/Addons.js";
 
 let sceneWidth = Math.max(0.9 * window.innerWidth, window.innerWidth - 200);
 let sceneHeight = window.innerHeight;
@@ -21,22 +19,36 @@ renderer.setSize(sceneWidth, sceneHeight);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
-// controls
-let controls = new OrbitControls(camera, renderer.domElement);
-controls.listenToKeyEvents(window);
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
 camera.position.z = 5;
 
+// map-like controls
+const controls = new MapControls(camera, renderer.domElement);
+controls.screenSpacePanning = true;
+controls.mouseButtons.RIGHT = null;
+
+
+// create objects
+
+const circle = new THREE.Shape();
+circle.absarc(0, 0, 2);
+
+const geometry = new THREE.ShapeGeometry(circle, 100);
+
+const material = new THREE.MeshBasicMaterial({
+  color: 0x00ff00,
+  side: THREE.DoubleSide,
+  depthWrite: false
+});
+
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+
+
+
 function animate() {
-  if (!paused) {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-  }
+
 
   renderer.render(scene, camera);
 }
